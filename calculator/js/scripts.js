658e31d6f91c0a213e4e58525e057d88161a0967
Julianne Soifer and Leah Leonard <leah@>
2018-05-30 19:50:39 -0700

    create simple calculator to add, subratct, multiply, divide using javascript.

---
+++ calculator/js/scripts.js
@@ -0,0 +1,52 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#addoutput").text(result);
+
+  });
+
+    $("form#subtract").submit(function(event) {
+      event.preventDefault();
+      var number1 = parseInt($("#subtract1").val());
+      var number2 = parseInt($("#subtract2").val());
+      var result = subtract(number1, number2);
+      $("#subtractoutput").text(result);
+  });
+
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#multiplyoutput").text(result);
+  });
+
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#divideoutput").text(result);
+});
+});
